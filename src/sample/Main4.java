8d6b0726414b7d642ebdf980d2652bce8d081f89
assassin_24 <83644019+huy24112001@users.noreply.github.com>
2021-11-03 00:54:58 +0700

    Update Main4.java

--- src/sample/Main4.java
+++ src/sample/Main4.java
@@ -57,7 +57,7 @@ public class Main4 extends Application {
         } );
         Optional<Pair<String, String>> result = dialog.showAndWait();
         result.ifPresent(userNamepassword -> {
-            System.out.println("Username="+userNamepassword.getKey()+", Password="+userNamepassword.getValue());
+            System.out.println("Username="+userNamepassword.getKey()+", Password="+userNamepassword.getValue()) ;
         });
     }
 }
